6ac8471d71b46caf40c50e24fb1c2ffc25cf8720
Bikram Kumar <coolbikram0@gmail.com>
2024-02-02 20:05:27 +0530

    basics done

--- app/src/main/java/com/bikram/notifier/MainActivity.java
+++ app/src/main/java/com/bikram/notifier/MainActivity.java
@@ -2,15 +2,8 @@ package com.bikram.notifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Build;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import android.app.NotificationManager;
-import android.app.NotificationChannel;
+
 import android.content.Intent;
-import android.app.PendingIntent;
-import android.app.AlarmManager;
-import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,68 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        /*Intent i = new Intent();
-        i.setClassName("com.bikram.flashlight", "com.bikram.flashlight.MainActivity");
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);*/
-        
-        
-        Intent launchNotifManager = new Intent(this, NotifManager.class);
-        sendBroadcast(launchNotifManager);
-        
-        
-        /*
-        
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-        
-      
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        builder.setContentTitle("Notification").setContentText("DO IT!");
-        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        builder.setContentIntent(pendingIntent).setAutoCancel(true);
-        
-        
-        //createNotificationChannel();
-        
-        NotificationManagerCompat.from(this).notify(1, builder.build());
-        MainActivity self = this;
         
         
         
-        Intent nextAlarmIntent = new Intent(this, MainActivity.class);
-        PendingIntent nextAlarmPIntent = PendingIntent.getActivity(this,0,nextAlarmIntent,0);
+        sendBroadcast(new Intent(this, NotifManager.class));
         
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, nextAlarmPIntent);
-       
-               
         
-        NotificationManagerCompat.from(self).notify(2, builder.build());
-                    
-       
-                
+        
+        
+    }
+    
 
-        */
-        
-        
-    }
-    
-    
-    /*
-    
-    private void createNotificationChannel() {
-        // return if API is below 26... NO SUPPORT
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
-        
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Notif name", NotificationManager.IMPORTANCE_DEFAULT);
-        channel.setDescription("Description of Channel");
-        getSystemService(NotificationManager.class).createNotificationChannel(channel);
-        
-    }
-    */
     
 }
--- app/src/main/java/com/bikram/notifier/NotifManager.java
+++ app/src/main/java/com/bikram/notifier/NotifManager.java
@@ -1,5 +1,10 @@
 package com.bikram.notifier;
 
+
+import android.os.Build;
+
+
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,53 +15,46 @@ import android.app.NotificationManager;
 import android.app.NotificationChannel;
 import android.content.Intent;
 import android.app.PendingIntent;
+import android.app.AlarmManager;
+
 
 import java.util.HashMap;
 
 
 public class NotifManager extends BroadcastReceiver {
     
-    String CHANNEL_ID = "MainChannel";
+    String CHANNEL_ID = "NormalPriorityChannel";
     
     HashMap<Long,String> notifList = new HashMap<Long,String>();
     
     
+    
+    private Context context;
+    private Intent intent;
+    
     @Override
-    public void onReceive(Context context, Intent intent1) {
+    public void onReceive(Context ctx, Intent _intent) {
+        context = ctx;
+        intent = _intent;
         
-        notifList.put(Long.valueOf(5), "Do it");
+        
+        
+        notifList.put(Long.valueOf(System.currentTimeMillis() + 1000), "Do it");
         notifList.put(Long.valueOf(9), "Now");
         notifList.put(Long.valueOf(7), "The time has come");
         
+        
+        
+        createNotificationChannel();
+        checkAllNotifs();
+        
+        /*
         Intent i = new Intent();
         i.setClassName("com.bikram.flashlight", "com.bikram.flashlight.MainActivity");
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
+        */
         
-        
-        
-        
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-        
-      
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        builder.setContentTitle("Notification").setContentText("DO IT!");
-        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        builder.setContentIntent(pendingIntent).setAutoCancel(true);
-        
-        
-        
-        
-        
-        
-        //createNotificationChannel();
-        
-        
-        NotificationManagerCompat.from(context).notify(1, builder.build());
         
         
         
@@ -73,16 +71,40 @@ public class NotifManager extends BroadcastReceiver {
     
     
     void checkAllNotifs() {
-        for (long i : notifList.keySet()) {
-            if (i < System.currentTimeMillis()) {
-                sendNotif();
+        // time is key, so add 1 at the time of setting if two notifs are on same time
+        for (long timeKey : notifList.keySet()) {
+            if (timeKey < System.currentTimeMillis()) {
+                sendNotif(timeKey);
             }
         }
     }
     
     
-    void sendNotif() {
+    void sendNotif(Long timeKey) {
         // build notif and notify
+        
+        Intent i = new Intent(context, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE);
+        
+      
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setContentTitle("Notification").setContentText(notifList.get(timeKey));
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(pendingIntent).setAutoCancel(true);
+        
+        
+        
+        
+        
+        
+        //createNotificationChannel();
+        
+        
+        NotificationManagerCompat.from(context).notify(timeKey.intValue(), builder.build());
+        
         
         
         
@@ -92,6 +114,29 @@ public class NotifManager extends BroadcastReceiver {
     
     void setAlarmForNextNotif() {
         
+        Intent nextAlarmIntent = new Intent(context, NotifManager.class);
+        PendingIntent nextAlarmPIntent = PendingIntent.getBroadcast(context,0,nextAlarmIntent,0);
+        
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, nextAlarmPIntent);
+       
     }
+    
+    
+    
+        
+    void createNotificationChannel() {
+        // return if API is below 26... NO SUPPORT
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
+        
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Normal Priority Channel", NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("For normal priority notifications.");
+        context.getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        
+    }
+    
+    
+    
+    
     
 }
